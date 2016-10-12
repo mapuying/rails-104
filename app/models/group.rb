e0ccce29840a18d2c11034fdad8a5d1b74c6b228
mapuying <mapuying@gmail.com>
2016-10-12 16:22:12 +0800

    join or quit group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
-  has_many :groups_relationships
-  has_many :members, through: :groups_relationships, source: :user
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
   validates :title, presence: true
 end
